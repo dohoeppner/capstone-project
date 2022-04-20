274e94e19565f41ac3c347323dd575222d940bd4
Dora Hoeppner <dora.hoeppner@gmail.com>
2022-04-20 16:55:38 +0200

    Remove console log

--- context/languagesContext.js
+++ context/languagesContext.js
@@ -1,14 +1,10 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useState } from "react";
 import languageMock from "../mock/languages";
 
 export const LanguagesContext = createContext();
 
 export default function LanguagesContextProvider({ children }) {
   const [languages, setLanguages] = useState(languageMock);
-
-  useEffect(() => {
-    console.log(languages);
-  });
 
   return (
     <LanguagesContext.Provider value={{ languages, setLanguages }}>
